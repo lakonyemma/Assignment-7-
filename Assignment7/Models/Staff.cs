81c3625b5b63198f073f4d6d7b2bc7228a2b8e39
Lakony Emmanuel <lakonyemmanuel92@gmail.com>
2026-09-07 11:54:32 +0300

    Add staff model

---
+++ Assignment7/Models/Staff.cs
@@ -0,0 +1,10 @@
+namespace Assignment7.Models
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
